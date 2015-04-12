8de1825739faae7f8c15432890a51641efff8260
Tim Zwart <timwaagh@gmail.com>
2015-04-12 23:13:43 +0200

    added a post approach route

--- helloworld.hs
+++ helloworld.hs
@@ -8,12 +8,15 @@ data HelloWorld = HelloWorld
 
 mkYesod "HelloWorld" [parseRoutes|
 / HomeR GET
+/approaches ApproachesR POST
 |]
 
 instance Yesod HelloWorld
 
 getHomeR :: Handler Html
 getHomeR = defaultLayout [whamlet|Hello World!|]
+
+postApproachesR = defaultLayout [whamlet|posted a new approach!|] 
 
 main :: IO ()
 main = warp 3000 HelloWorld
